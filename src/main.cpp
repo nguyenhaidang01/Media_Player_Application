e462fd942721bb1ca4620c630580685658f58cd0
nguyenhaidang01 <dang.nguyenhai208@gmail.com>
2023-09-23 21:50:55 +0700

    Update Linux version

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "include/Player.h"
+#include "include/Playlistmodel.h"
+#include "include/Translation.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    qRegisterMetaType<QMediaPlaylist*>("QMediaPlaylist*");
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    Translation translator;
+    translator.selectLanguage("vn");
+
+    Player player;
+    engine.rootContext()->setContextProperty("myModel",player.m_playlistModel);
+    engine.rootContext()->setContextProperty("player",player.m_player);
+    engine.rootContext()->setContextProperty("utility",&player);
+    engine.rootContext()->setContextProperty("translator",&translator);
+    player.m_playlist->setPlaybackMode(QMediaPlaylist::Loop);
+
+    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
